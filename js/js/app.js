dd417ec079861dd22626336ae1ffe8044b3cee7d
Kacperrrr2 <144324382+Kacperrrr2@users.noreply.github.com>
2024-02-02 09:52:05 +0100

    Add files via upload

---
+++ js/js/app.js
@@ -0,0 +1,72 @@
+// /*zadanie 1*/
+// console.log("Hello World");
+// /*zadanie 2*/
+// zmienna=12
+// console.log(typeof zmienna)
+// console.log("Język Java Script")
+// console.log(typeof "Język Java Script")
+// /*zadanie 3*/
+// a=21;
+// b=17;
+// alert(a+b)
+// /*zadanie 4*/
+// cos=prompt("Podaj liczbe")
+// console.log(parseInt(cos))
+/*zadanie 5*/
+/*
+var a=15;
+var b=5;
+var c=a+b;
+document.getElementById('wynik').innerHTML=c;
+*/
+/*zadanie6*/
+/*
+ var a=73
+var b=123
+document.getElementById('wynik').innerHTML="<h1>"+ (a+b) +"</h1>";
+*/
+
+/*zadanie7*/
+
+//  var a=73
+// var b=123
+// document.getElementById('wynik').innerHTML=`<h1> ${(a+b)}</h1>`;
+
+/*zadanie 8*/
+// var przypisz="";
+// for (i=1;i<101;i++)
+// {
+//   przypisz+=`${i},`;
+// }
+// document.getElementById('wynik').innerHTML=przypisz
+// zadanie 9
+// a=prompt("Wyswietl jakaś liczba");
+// a=parseInt(a);
+// const coss= a>0? `${a} jest dodadania`:`${a} jest ujemna`;
+// document.getElementById('opis').innerHTML=coss;
+// zadanie 12
+//  a=prompt("Wyswietl jakaś liczba");
+//  a=parseInt(a);
+//  if (a>0) {
+//
+//    document.getElementById('opis').innerHTML = `${a} jest dodadania`;
+//  }
+//  else if (a===0){
+//    document.getElementById('opis').innerHTML = `${a} jest zerem`;
+//  }
+//  else{
+//    document.getElementById('opis').innerHTML = `${a} jest ujemna`;
+//  }
+// zadanie 13
+// function wyswietl(){
+//   document.getElementById('opis').innerHTML="Witaj świecie!";
+//   document.getElementById('przycisk').style.display= "none";
+// }
+// zadanie 14
+//
+// document.getElementById('wynikkk').innerHTML=0;
+//  function  zmieniaj() {
+//      var dd= document.getElementById('cosss').value;
+//  console.log(dd);
+//  document.getElementById('wynikkk').innerHTML=dd;
+ }
